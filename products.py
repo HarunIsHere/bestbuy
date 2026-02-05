ae4e5c71a771c8c0dcc810198b80f4c38eb927e7
Harun Ayartürk <harunayarturk@gmail.com>
2026-02-05 01:12:58 +0100

    Create Product class

---
+++ products.py
@@ -0,0 +1,72 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        if not isinstance(name, str) or name.strip() == "":
+            raise Exception("Product name must be a non-empty string")
+        if price < 0:
+            raise Exception("Product price cannot be negative")
+        if quantity < 0:
+            raise Exception("Product quantity cannot be negative")
+
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+        if self.quantity == 0:
+            self.active = False
+
+    def get_quantity(self):
+        return self.quantity
+
+    def set_quantity(self, quantity):
+        if quantity < 0:
+            raise Exception("Quantity cannot be negative")
+
+        self.quantity = quantity
+        if self.quantity == 0:
+            self.deactivate()
+        else:
+            self.activate()
+
+    def is_active(self):
+        return self.active
+
+    def activate(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def show(self):
+        print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
+
+    def buy(self, quantity):
+        if quantity <= 0:
+            raise Exception("Buy quantity must be greater than 0")
+        if not self.is_active():
+            raise Exception("Cannot buy an inactive product")
+        if quantity > self.quantity:
+            raise Exception("Not enough quantity in stock")
+
+        total_price = self.price * quantity
+        self.set_quantity(self.quantity - quantity)
+        return total_price
+
+
+def main():
+    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+    mac = Product("MacBook Air M2", price=1450, quantity=100)
+
+    print(bose.buy(50))
+    print(mac.buy(100))
+    print(mac.is_active())
+
+    bose.show()
+    mac.show()
+
+    bose.set_quantity(1000)
+    bose.show()
+
+
+if __name__ == "__main__":
+    main()
